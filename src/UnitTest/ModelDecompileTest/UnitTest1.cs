6f687fd65080938a4e8fcdc8b0a8367c23c4771b
Anna <jiafeng5513@outlook.com>
2018-10-20 13:08:19 +0800

    给出了图约减算法

---
+++ src/UnitTest/ModelDecompileTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtobufTools;
+
+namespace ModelDecompileTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ProtoTools.Decompile();
+        }
+    }
+}
--- utils/mnist/mnist_deep.py
+++ utils/mnist/mnist_deep.py
@@ -4,9 +4,13 @@ from __future__ import print_function
 
 import argparse
 import sys
-
-from tensorflow.examples.tutorials.mnist import input_data
+import input_data
+#from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.python.framework import graph_util
+#official/mnist/dataset.py from tensorflow/models
+
+
+
 import tensorflow as tf
 from plot_weight import plot_conv_weights,plot_conv_output
 import GlobalVariable
