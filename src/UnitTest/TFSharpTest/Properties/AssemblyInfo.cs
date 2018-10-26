300def0abf3f55daa209ad3c610d1f7bf1644a5a
Anna <jiafeng5513@outlook.com>
2018-10-26 23:47:54 +0800

    内置了TFSharp,使用socket通信隔离TFSharp和托管程序集,目前所有项目都采用x64debug 模式,新增了一个bugreport,找回了依赖关系图

---
+++ src/UnitTest/TFSharpTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TFSharpTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TFSharpTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce55ca50-8f9d-438d-bafa-f2eb13a0d57c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
